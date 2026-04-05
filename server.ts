67dfe1a153b9cdd7ada98471bdd58ef387eb5abb
cassiolopesonline-bit <cassiolopes.online@gmail.com>
2026-04-05 14:41:24 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 10000;
 
   app.use(cors());
   app.use(express.json({ limit: '50mb' }));
