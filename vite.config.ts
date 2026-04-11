e54e38630d73bf736fee408a4cbf62bb7351a421
cassiolopesonline-bit <cassiolopes.online@gmail.com>
2026-04-11 16:07:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  // ISSO AQUI DIZ AO VITE ONDE ESTÁ O INDEX.HTML
+  root: 'src',
+  build: {
+    // ISSO DIZ ONDE SALVAR O SITE PRONTO
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
-    // Isso aqui libera o acesso total no Render
     allowedHosts: true,
     host: true,
     port: 10000
